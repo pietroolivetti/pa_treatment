41dcccf51ddf1f56723423c84857af93dc059bd1
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-09-29 21:36:38 +0100

    Create diario_de_humor.py
    text only added to test

---
+++ diario_de_humor.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("""Você também pode manter um registro de seus sentimentos gerais ao longo do dia, completando o Formulário do Seu Registro Diário de Humor (Formulário 2.2) no final de cada dia. Use uma escala de 10 pontos para avaliar seus níveis diários de ansiedade, depressão (ou seja, quão triste, desanimado ou sem energia você se sente) e o quanto você se preocupa em ter um ataque de pânico (ou seja, o quanto o pânico está em sua mente, o quanto você está preocupado com a possibilidade de entrar em pânico).
+
+Para todas as avaliações, 0 = nenhum, 5 = moderado e 10 = ansiedade, depressão e preocupação extrema com o pânico. Essas avaliações são baseadas em como você se sentiu, em média, durante o dia. Em outras palavras, considerando o dia inteiro e combinando todos os altos e baixos ao longo do dia, qual foi a quantidade média de ansiedade, depressão e preocupação com o pânico que você experimentou?
+
+Ao longo de várias semanas, muitas vezes, tendências se tornam evidentes. Um exemplo disso é a maneira como os estados de humor flutuam em relação à frequência dos ataques de pânico. Será importante aprender essas informações para tornar seu tratamento o mais eficaz possível.
+""")
